34e2293b80487933d71d35966983fd42ba18736a
Pietro Grandi <pietro@knipster.com>
2019-03-18 19:20:31 +0100

    6 tests left... I may actually finish the assignment o_O

--- assignments/kvstore/src/main/scala/kvstore/Replica.scala
+++ assignments/kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,36 +1,49 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, PoisonPill, Props, SupervisorStrategy, Terminated}
 import kvstore.Arbiter._
+
 import scala.collection.immutable.Queue
 import akka.actor.SupervisorStrategy.Restart
+
 import scala.annotation.tailrec
-import akka.pattern.{ ask, pipe }
-import akka.actor.Terminated
+import akka.pattern.{ask, pipe}
+
 import scala.concurrent.duration._
-import akka.actor.PoisonPill
-import akka.actor.OneForOneStrategy
-import akka.actor.SupervisorStrategy
+import scala.language.postfixOps
 import akka.util.Timeout
 
 object Replica {
+
   sealed trait Operation {
     def key: String
+
     def id: Long
   }
+
   case class Insert(key: String, value: String, id: Long) extends Operation
+
   case class Remove(key: String, id: Long) extends Operation
+
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
+
   case class OperationAck(id: Long) extends OperationReply
+
   case class OperationFailed(id: Long) extends OperationReply
+
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
+
+  case class PersistenceData(key: String, valueOption: Option[String], client: ActorRef)
+
+  case class PersistenceTimeout(persistenceData: PersistenceData, id: Long)
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
-class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
+class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor with ActorLogging {
+
   import Replica._
   import Replicator._
   import Persistence._
@@ -39,55 +52,108 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
+  // waiting for PersistenceAck
+  var pending = Map.empty[Long, PersistenceData]
+
+  val persistence = context.system.actorOf(persistenceProps)
+  val scheduler = context.system.scheduler
+  val TIMEOUT = 100 millis
+
+  override val supervisorStrategy = OneForOneStrategy(maxNrOfRetries = 5) {
+    case _: Exception =>
+      SupervisorStrategy.Restart
+  }
 
   arbiter ! Join
 
   def receive = {
-    case JoinedPrimary   => context.become(leader)
+    case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica(0L))
   }
 
-  def insertOrRemove(key: String, optionValue: Option[String]) = optionValue match {
-    case Some(value) =>
-      kv = kv + ((key, value))
-    case None =>
-      kv = kv - key
+  def insertOrRemove(key: String, valueOption: Option[String], id: Long) = {
+    valueOption match {
+      case Some(value) =>
+        kv = kv + ((key, value))
+      case None =>
+        kv = kv - key
+    }
+  }
+
+  def persist(persistenceData: PersistenceData, id: Long) = {
+    pending = pending + ((id, persistenceData))
+    scheduler.scheduleOnce(TIMEOUT, self, PersistenceTimeout(persistenceData, id))
+    persistence ! Persist(persistenceData.key, persistenceData.valueOption, id)
   }
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
     case Insert(key, value, id) =>
-      insertOrRemove(key, Some(value))
-      sender ! OperationAck(id)
+      insertOrRemove(key, Some(value), id)
+      persist(PersistenceData(key, Some(value), sender), id)
+
+    // TODO: sender ! OperationAck(id)
     case Remove(key, id) =>
-      insertOrRemove(key, None)
-      sender ! OperationAck(id)
-    case Get(key,id) =>
+      insertOrRemove(key, None, id)
+      persist(PersistenceData(key, None, sender), id)
+
+    case Persisted(key, seq) =>
+      (pending get seq) match {
+        case Some(persistenceData) =>
+          persistenceData.client ! OperationAck(seq) // id = seq for 2nd replicas
+          pending = pending - seq
+        case None =>
+          log.error(s"Got Persisted for ${key} - ${seq} but no pending found")
+      }
+
+    case PersistenceTimeout(persistenceData, seq) =>
+      log.debug(s"${seq} for ${persistenceData.key} was PersistenceTimeout")
+      if (pending contains seq) {
+        persist(persistenceData, seq)
+      }
+
+    case Get(key, id) =>
       sender ! GetResult(key, kv get key, id)
+
     case _ =>
   }
 
   /* TODO Behavior for the replica role. */
   def replica(expectedSeq: Long): Receive = {
-    case Get(key,id) =>
+    case Get(key, id) =>
       sender ! GetResult(key, kv get key, id)
+
     case Snapshot(key, valueOption, seq) =>
       if (seq == expectedSeq) {
-        insertOrRemove(key, valueOption)
-        sender ! SnapshotAck(key, seq)
-        context.become(replica(expectedSeq+1))
+        insertOrRemove(key, valueOption, seq)
+        persist(PersistenceData(key, valueOption, sender), seq)
+        context.become(replica(expectedSeq + 1))
       } else if (seq < expectedSeq) {
         sender ! SnapshotAck(key, seq)
-        context.become(replica(seq+1))
+        context.become(replica(seq + 1))
       }
-      // if greater than expected, ignore it
+    // if greater than expected, ignore it
 
+    case Persisted(key, seq) =>
+      (pending get seq) match {
+        case Some(persistenceData) =>
+          persistenceData.client ! SnapshotAck(key, seq) // id = seq for 2nd replicas
+          pending = pending - seq
+        case None =>
+          log.error(s"Got Persisted for ${key} - ${seq} but no pending found")
+      }
+
+    case PersistenceTimeout(persistenceData, seq) =>
+      log.debug(s"${seq} for ${persistenceData.key} was PersistenceTimeout")
+      if (pending contains seq) {
+        persist(persistenceData, seq)
+      }
     case _ =>
   }
 
